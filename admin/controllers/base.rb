f1d9650adf807421cab2466fd93d8af2cc0aebee
funnythingz <funnythingz69+github@gmail.com>
2015-01-23 23:28:12 +0900

    padrino generate admin and migration

---
+++ admin/controllers/base.rb
@@ -0,0 +1,5 @@
+HelloPadrino::Admin.controllers :base do
+  get :index, :map => "/" do
+    render "base/index"
+  end
+end
--- config/apps.rb
+++ config/apps.rb
@@ -33,4 +33,6 @@ Padrino.configure_apps do
 end
 
 # Mounts the core application for this project
+
+Padrino.mount("HelloPadrino::Admin", :app_file => Padrino.root('admin/app.rb')).to("/admin")
 Padrino.mount('HelloPadrino::App', :app_file => Padrino.root('app/app.rb')).to('/')
